d9a063f75a8f4f4e7d346fb3932d4e8e731a5f6b
martin.seller@schibsted.com <martin.seller@schibsted.com>
2024-01-10 08:19:47 +0100

    starting over with own config

---
+++ lua/nyxonios/init.lua
@@ -0,0 +1,22 @@
+require("nyxonios.set")
+require("nyxonios.remap")
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  
+}
+
+require('lazy').setup("nyxonios.plugins")
+
