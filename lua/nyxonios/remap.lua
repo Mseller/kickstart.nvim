6f16bf13f136b9b4df5fb54e47f941085228e7c7
martin.seller@schibsted.com <martin.seller@schibsted.com>
2024-02-20 08:24:19 +0100

    chore(neo-tree): close if open

--- lua/nyxonios/remap.lua
+++ lua/nyxonios/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fe", '<cmd>Neotree position=right<cr>')
+vim.keymap.set("n", "<leader>fe", '<cmd>Neotree position=left toggle<cr>')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -7,4 +7,3 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
